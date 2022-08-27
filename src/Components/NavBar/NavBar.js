5b872869b0a4e4ad0815a09c64df5c76779ae2b8
Adithyan M.S <adithyan2ms@gmail.com>
2022-08-27 23:08:27 +0530

    basic home page css done

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./NavBar.css"
+function NavBar() {
+  return (
+    <div className='navbar'>
+        <img className='logo' src="https://th.bing.com/th/id/R.4d30a09d38e6dfe3feaa31920a680108?rik=GnWkgmKvj5nwNg&riu=http%3a%2f%2fwww.cultjer.com%2fimg%2fug_photo%2f2016_09%2f76248820160921034356.jpg&ehk=grD%2fdqb%2b43FMvO3KHmAdPwgUvOikuZkwrJQwMPdCHUM%3d&risl=&pid=ImgRaw&r=0" alt="netflix logo" />
+        <img className='avatar' src="https://th.bing.com/th/id/OIP.uMjFSP54dulnFF-KSLWBbAHaHa?w=176&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7" alt="nav avatar" />
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/Components/RowPost/RowPost.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './RowPost.css'
+function RowPost() {
+  return (
+    <div className='row'>
+        <h2>Title</h2>
+        <div className="posters">
+            <img className='poster' src="https://images.hdqwalls.com/wallpapers/1917-movie-2020-9k.jpg" alt="" />
+            <img className='poster' src="https://images.hdqwalls.com/wallpapers/1917-movie-2020-9k.jpg" alt="" />
+            <img className='poster' src="https://images.hdqwalls.com/wallpapers/1917-movie-2020-9k.jpg" alt="" />
+            <img className='poster' src="https://images.hdqwalls.com/wallpapers/1917-movie-2020-9k.jpg" alt="" />
+            <img className='poster' src="https://images.hdqwalls.com/wallpapers/1917-movie-2020-9k.jpg" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default RowPost
